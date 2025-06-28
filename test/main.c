736158e92a5402e6ebdbb0f5c69c362ed3073ddf
Hannes Leonhartsberger <hannes.leonhartsberger@gmail.com>
2025-06-29 00:39:43 +0200

    Added tests (currently failing)

---
+++ test/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include "library.h"
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        return 1;
+    }
+
+    printf("%s", argv[1]);
+    hello();
+
+    return 0;
+}
