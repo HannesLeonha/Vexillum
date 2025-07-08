150665f189f615e510f1d16c360b822037b11e1f
Hannes Leonhartsberger <hannes.leonhartsberger@gmail.com>
2025-07-09 01:36:49 +0200

    Fixed tests

--- test/main.c
+++ test/main.c
@@ -3,10 +3,11 @@
 
 int main(int argc, char **argv) {
     if (argc == 1) {
+        printf("Test failed");
         return 1;
     }
 
-    printf("%s", argv[1]);
+    printf("Success: %s", argv[1]);
     hello();
 
     return 0;
