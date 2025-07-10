88f971bcd3449f7080a0b9b74e227351f567ed15
Hannes Leonhartsberger <hannes.leonhartsberger@gmail.com>
2025-07-10 23:33:00 +0200

    Defined public interface of the library

--- src/Vexillum.c
+++ src/Vexillum.c
@@ -1,7 +1,51 @@
 ﻿#include "Vexillum.h"
 
-#include <stdio.h>
+#include <stdbool.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+void enable_usage_message_on_parse_error(char* program_name, char* program_description) {
+  return;
+}
+
+void enable_error_code_on_parse_error() {
+
+}
+
+
+struct vexillum_error_code register_flag(char short_format, char* long_format, char* flag_description, bool has_argument) {
+  return;
+}
+
+struct vexillum_error_code unregister_flag(char short_format) {
+  return;
+}
+
+void clear_flags() {
+  return;
+}
+
+
+struct vexillum_error_code parse_arguments(int argc, char **argv) {
+  return;
+}
+
+
+int argument_amount() {
+  return;
+}
+
+char* get_argument(int argument_number) {
+  return;
+}
+
+bool is_flag_set() {
+  return;
+}
+
+char* get_flag_argument() {
+  return;
+}
+
+
+char* get_error_string(struct vexillum_error_code error_code) {
+  return;
 }
--- src/Vexillum.h
+++ src/Vexillum.h
@@ -1,6 +1,26 @@
 ﻿#ifndef VEXILLUM_LIBRARY_H
 #define VEXILLUM_LIBRARY_H
 
-void hello(void);
+#include <stdbool.h>
+
+struct vexillum_error_code {
+    char code;
+};
+
+void enable_usage_message_on_parse_error(char* program_name, char* program_description);
+void enable_error_code_on_parse_error();
+
+struct vexillum_error_code register_flag(char short_format, char* long_format, char* flag_description, bool has_argument);
+struct vexillum_error_code unregister_flag(char short_format);
+void clear_flags();
+
+struct vexillum_error_code parse_arguments(int argc, char **argv);
+
+int argument_amount();
+char* get_argument(int argument_number);
+bool is_flag_set();
+char* get_flag_argument();
+
+char* get_error_string(struct vexillum_error_code error_code);
 
 #endif //VEXILLUM_LIBRARY_H
